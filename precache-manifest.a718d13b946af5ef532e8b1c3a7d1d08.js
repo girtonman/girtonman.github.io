5ce63b56831a93ec95271594595b0d0c51d2fee9
girtonman <girtonman@gmail.com>
2020-08-09 20:48:54 -0500

    Updates

---
+++ precache-manifest.a718d13b946af5ef532e8b1c3a7d1d08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcacbbb669e68b4620164ff1d007de31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b71767083847eabda83",
+    "url": "/static/css/main.40b5acf3.chunk.css"
+  },
+  {
+    "revision": "eef2f8d259a917904de9",
+    "url": "/static/js/2.82f21562.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.82f21562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b71767083847eabda83",
+    "url": "/static/js/main.90ad3b19.chunk.js"
+  },
+  {
+    "revision": "0d00e1564ef738da4bf8",
+    "url": "/static/js/runtime-main.1cbc3380.js"
+  }
+]);
